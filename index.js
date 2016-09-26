2926d8f6d5ccc47e938f9f8d32fc147dbc91a45d
Cameron Wilcox <Cam@Camerons-MacBook-Pro.local>
2016-09-26 11:25:45 -0700

    Morgage Calc First

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+$(document).ready(function(){
+
+$('#balanceInput').keyup(function() {
+   $(this).val(function(i,v) {
+     return '$' + v.replace('$','');
+   });
+ });
+
+
+	$("#calculateButton").click(function(click){
+
+		this.blur();
+
+		var loanBalance = $("#balanceInput").val().split("$").pop();
+		var interestRate = $("#rateInput").val();
+		var loanTerm = $("#termInput").val();
+		var periodValue = $("#periodSelect").val();
+		var findIfZero = loanBalance * interestRate * loanTerm;
+
+		if (findIfZero == 0) {
+			alert("Make sure all of the fields are filled in.");
+		} 
+		else
+		{
+			if (!$.isNumeric(loanBalance)) {
+				alert("Please check that you have entered a valid Loan Balance.");
+			} 
+			else
+			{
+				if (interestRate>100) {
+					alert("Your interest rate percentage is too high.");
+				} else
+				{
+					var numberOfPayments = loanTerm * periodValue;
+					var monthlyInterestRate = (interestRate / 100)/periodValue;
+					var compoundedIntestRate = Math.pow((1+monthlyInterestRate),numberOfPayments);
+					var interestQuotient = (monthlyInterestRate * compoundedIntestRate) / (compoundedIntestRate - 1);
+					var monthlyPayment = (loanBalance * interestQuotient);
+					$("#expectedOutput").text("Your monthly payment: $" + monthlyPayment.toFixed(2));
+				}
+			}
+		}
+	});
+		
+
+});
+
+
+
+
+
+
+
+
