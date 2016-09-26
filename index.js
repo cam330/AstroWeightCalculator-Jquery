4ac4947f08fa32ee84d98df460140e413589be8d
cam330 <wilcoxcam@gmail.com>
2016-09-26 16:26:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-
-
-
 $(document).ready(function(){
 
-$("#calculateButton").prop("disabled", true);
+$("#calculateButton").prop("disabled", true); //Sets button to disabled at page load
 
-$('#balanceInput').keyup(function() {
-   $(this).val(function(i,v) {
-     return '$' + v.replace('$','');
-   });
- });
+$('#balanceInput').focus(); //Sets the balance to selected on page load
 
-$("input").keyup(function(){
+	
+	$('#balanceInput').keyup(function() { //Sets a dollar sign to beginning of balance field
 
-	var findIfZero =  $("#balanceInput").val().split("$").pop().length * $("#rateInput").val().length * $("#termInput").val().length;
-	if (findIfZero > 0) {
-		$("#calculateButton").prop("disabled", false);
-	}
-	else
-	{
-		$("#calculateButton").prop("disabled", true);
-	}
-});
+	   $(this).val(function(i,v) {
+	     return '$' + v.replace('$','');
+	   });
+
+	 });
 
 
-	$("#calculateButton").click(function(click){
+	$("input").keyup(function(){ //Checks to see when the fields are all populated
+
+		var findIfZero =  $("#balanceInput").val().split("$").pop().length * $("#rateInput").val().length * $("#termInput").val().length;
+		if (findIfZero > 0) {
+			$("#calculateButton").prop("disabled", false);
+		}
+		else
+		{ //If All of the fields are populated, enable the calculate button
+			$("#calculateButton").prop("disabled", true);
+		}
+	});
+
+
+	$("#calculateButton").click(function(click){ //Button function to do calculation
 
 		this.blur();
 
@@ -34,45 +37,43 @@ $("input").keyup(function(){
 		var periodValue = $("#periodSelect").val();
 		var findIfZero = loanBalance * interestRate * loanTerm;
 
-		if (findIfZero == 0) {
-			alert("Make sure all of the fields are filled in.");
+		if (findIfZero == 0) { //If there are valid numbers in the fields
+			alert("Make sure you have entered valid numbers.");
 		} 
 		else
 		{
-			if (!$.isNumeric(loanBalance)) {
+			if (!$.isNumeric(loanBalance)) { //If you have entered a letter in the loan balance field
 				alert("Please check that you have entered a valid Loan Balance.");
+				$("#loan").addClass("has-error");
 			} 
 			else
-			{
+			{ //If your percentage is above 100%
+				$("#loan").removeClass("has-error");
 				if (interestRate>100) {
 					alert("Your interest rate percentage is too high.");
+					$("#rate").addClass("has-error");
 				} 
 				else
-				{
-					var numberOfPayments = loanTerm * periodValue;
-					var monthlyInterestRate = (interestRate / 100)/periodValue;
-					var compoundedIntestRate = Math.pow((1+monthlyInterestRate),numberOfPayments);
-					var interestQuotient = (monthlyInterestRate * compoundedIntestRate) / (compoundedIntestRate - 1);
-					var monthlyPayment = (loanBalance * interestQuotient);
-					$("#expectedOutput").text("Your monthly payment: $" + monthlyPayment.toFixed(2));
+				{ //If your loan term years are too high
+					$("#rate").removeClass("has-error");
+					if (loanTerm > 1343) {
+						alert("Your loan term years must be between 1-1343.");
+					$("#term").addClass("has-error");
+					}
+					else
+					{ //If everything is ok it will run the function
+						$("#term").removeClass("has-error");
+						var numberOfPayments = loanTerm * periodValue;
+						var monthlyInterestRate = (interestRate / 100)/periodValue;
+						var compoundedIntestRate = Math.pow((1+monthlyInterestRate),numberOfPayments);
+						var interestQuotient = (monthlyInterestRate * compoundedIntestRate) / (compoundedIntestRate - 1);
+						var monthlyPayment = (loanBalance * interestQuotient);
+						
+						$("#expectedOutput").text("Your monthly payment: $" + monthlyPayment.toFixed(2));
+					}
 				}
 			}
 		}
 	});
 		
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
